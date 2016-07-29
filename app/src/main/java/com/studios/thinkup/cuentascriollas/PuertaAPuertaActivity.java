083e48d9df9ded5b4bf4c1af697590c1168fb10b
fcostazini <fcostazini@conexia.com>
2016-07-29 12:33:21 -0300

    Initial commit

---
+++ app/src/main/java/com/studios/thinkup/cuentascriollas/PuertaAPuertaActivity.java
@@ -0,0 +1,25 @@
+package com.studios.thinkup.cuentascriollas;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PuertaAPuertaActivity extends DrawerMain {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void setView() {
+        setContentView(R.layout.activity_puerta_apuerta);
+    }
+
+
+
+}
